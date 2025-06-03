ac4ee272a6066d05f42657856d7d6f6c56b4d1ca
staugi <ugnius@dev.stasaitis.me>
2025-06-03 16:01:51 +0300

    Starship

--- home/home.nix
+++ home/home.nix
@@ -42,12 +42,6 @@
 
   programs.starship = {
     enable = true;
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
   };
 
   home.file = {
