8d62669b793fe7ccbad6cd1cc1c2a26301368410
staugi <ugnius@dev.stasaitis.me>
2025-08-24 01:52:51 +0300

    Nasys ssh

--- hosts/nasys/configuration.nix
+++ hosts/nasys/configuration.nix
@@ -4,4 +4,22 @@
   ];
 
   networking.hostName = "Nasys";
+
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+
+  users.users.nasys = {
+     isNormalUser = true;
+     description = "NAS";
+     extraGroups = [ "wheel" ];
+  };
 }
