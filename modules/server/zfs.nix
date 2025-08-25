8d82fa044aab819ff57fd5cd5822c4de52b49055
staugi <ugnius@dev.stasaitis.me>
2025-08-25 23:12:25 +0300

    Nasys zfs mnt dir

--- modules/server/zfs.nix
+++ modules/server/zfs.nix
@@ -5,4 +5,16 @@
   ];
   boot.zfs.forceImportRoot = false;
   networking.hostId = "ee696dc2";
+
+  systemd.tmpfiles.settings = {
+    "nas_mount_dir" = {
+      "/mnt/nas" = {
+        d = {
+          group = "root";
+          mode = "0755";
+          user = "root";
+        };
+      };
+    };
+  };
 }
