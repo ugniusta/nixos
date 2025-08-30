66ddfe4f6b9e8f2c48678de67992d3ecaee77943
staugi <ugnius@dev.stasaitis.me>
2025-08-30 19:20:36 +0300

    Nasys unbrick? 2

--- hosts/nasys/configuration.nix
+++ hosts/nasys/configuration.nix
@@ -2,6 +2,7 @@ let username = "nasys";
 in { config, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
+    ../../modules/nvidia
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_6_12_hardened;
@@ -13,7 +14,7 @@ in { config, pkgs, ... }: {
     ports = [ 22 ];
     settings = {
       PasswordAuthentication = true;
-      AllowUsers = [ username ]; # Allows all users by default. Can be [ "user1" "user2" ]
+      AllowUsers = [ "${username}" ]; # Allows all users by default. Can be [ "user1" "user2" ]
       UseDns = true;
       X11Forwarding = false;
       PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
@@ -22,9 +23,9 @@ in { config, pkgs, ... }: {
 
   users.users."${username}" = {
      isNormalUser = true;
-     description = "NAS";
+     description = "Main NAS user";
      extraGroups = [ "wheel" ];
-     openssh.authorizedKeys.keyFiles = [ "/etc/nixos/secrets/${username}/ssh/nasys.pub" ]; # TODO: Path var
+     openssh.authorizedKeys.keyFiles = [ "/etc/nixos/secrets/${username}/ssh/id_rsa_legion-5_nasys.pub" ]; # TODO: Path var
   };
-  nix.settings.trusted-users = [ username ];
+  nix.settings.trusted-users = [ "${username}" ];
 }
