30b280d3ad4c8afe6abcfb4b8e9aa20d12bb0326
staugi <ugnius@dev.stasaitis.me>
2025-08-30 20:37:09 +0300

    Rotated secrets - WHOOPS

--- hosts/nasys/configuration.nix
+++ hosts/nasys/configuration.nix
@@ -2,7 +2,6 @@ let username = "nasys";
 in { config, pkgs, ... }: {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/nvidia
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_6_12_hardened;
@@ -13,19 +12,19 @@ in { config, pkgs, ... }: {
     enable = true;
     ports = [ 22 ];
     settings = {
-      PasswordAuthentication = true;
-      AllowUsers = [ "${username}" ]; # Allows all users by default. Can be [ "user1" "user2" ]
+      PasswordAuthentication = false;
+      AllowUsers = [ "${username}" ];
       UseDns = true;
       X11Forwarding = false;
-      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+      PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
     };
   };
 
-  users.users."${username}" = {
+  users.users.${username} = {
      isNormalUser = true;
      description = "Main NAS user";
      extraGroups = [ "wheel" ];
-     openssh.authorizedKeys.keyFiles = [ "/etc/nixos/secrets/${username}/ssh/id_rsa_legion-5_nasys.pub" ]; # TODO: Path var
+     openssh.authorizedKeys.keyFiles = [ "/etc/nixos/secrets/${username}/ssh/legion-5_nasys@Nasys.pub" ]; # TODO: Path var
   };
   nix.settings.trusted-users = [ "${username}" ];
 }
